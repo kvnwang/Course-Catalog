254e6f0c5b0bbe5886f9f399e6405b636e816e41
Kevin <kwang30@gmail.com>
2017-10-09 22:08:04 -0400

    added tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,32 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Course Catalog"
   end
+
+  test "should get subjects" do
+    get subjects_url
+    assert_response :success
+    assert_select "title", "Subjects | Course Catalog"
+  end
+
+
+    test "should get courses" do
+      get courses_url
+      assert_response :success
+      assert_select "title", "Courses | Course Catalog"
+    end
+
+
+    test "should get instructors" do
+      get instructors_url
+      assert_response :success
+      assert_select "title", "Instructors | Course Catalog"
+
+    end
+
+
+
+
 
 end
