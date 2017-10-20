c2466c30b03e77360b5dadd01fa5b0d28eaf5d8a
Kevin <kwang30@gmail.com>
2017-10-20 13:52:12 -0400

    int query

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -3,6 +3,6 @@ class Enrollment < ApplicationRecord
   belongs_to :course
 
   def self.search(search)
-    return Enrollment.where("user_id = ", search)
+    return Enrollment.where("user_id = ? ", search)
   end
 end
