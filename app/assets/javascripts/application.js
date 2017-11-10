78e078e3027dfad495aee05626ca8276fe01f091
Kevin <kwang30@gmail.com>
2017-11-10 16:37:01 -0500

    edited

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,15 +15,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-$(function () {
-  $("#search").on('keyup', function () {
-    $.get($("#searches_search").attr("action"), $("#searches_search").serialize(), null, "script");
-    return false;
-  });
-});
-$(function () {
-  $("#browse").on('change', function(){
-    $.get($("#searches_search").attr("action"), $("#searches_search").serialize(), null, "script");
-    return false;
-  });
-});
