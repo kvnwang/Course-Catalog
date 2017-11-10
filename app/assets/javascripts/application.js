a62395fb2d929fbc66de6642504b5d7d3b096812
Kevin <kwang30@gmail.com>
2017-11-10 13:03:33 -0500

    updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,18 @@
 // about supported directives.
 //
 //= require jquery
-//= require rails-ujs
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(function () {
+  $("#search").on('keyup', function () {
+    $.get($("#searches_search").attr("action"), $("#searches_search").serialize(), null, "script");
+    return false;
+  });
+});
+$(function () {
+  $("#browse").on('change', function(){
+    $.get($("#searches_search").attr("action"), $("#searches_search").serialize(), null, "script");
+    return false;
+  });
+});
