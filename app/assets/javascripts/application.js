1000ff827a280a052add57ee6f74e7ae152e8518
Kevin <kwang30@gmail.com>
2017-11-10 02:54:16 -0500

    edits

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,17 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+$(function () {
+  $('#searches_search input').keyup(function () {
+    $.get($('#searches_search').attr('action'),
+    $('#searches_search').serialize(), null, 'script');
+    return false;
+  });
+});
+$(function () {
+  $("#browse").on('change', function(){
+    $.get($("#searches_search").attr("action"), $("#searches_search").serialize(), null, "script");
+    return false;
+  });
+});
