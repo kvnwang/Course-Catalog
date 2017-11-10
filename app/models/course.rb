e4474e6d783b66cb99f2f3c43bf6b6295f2c56cc
Kevin <kwang30@gmail.com>
2017-11-10 13:56:19 -0500

    update

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,10 +5,8 @@ class Course < ApplicationRecord
 
   def self.search(search, browse)
     if search.present? && browse.present?
-      search=search.downcase
       course=Course.where("name LIKE ?", "%#{search}%").merge (Subject.find_by subject_id: browse).courses
     elsif search.present? && !browse.present?
-      search=search.downcase
       course=Course.where("name LIKE ?", "%#{search}%")
     elsif !search.present? && browse.present?
       course=(Subject.find_by subject_id: browse).courses
